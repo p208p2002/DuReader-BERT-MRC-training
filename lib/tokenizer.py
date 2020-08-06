718ae21c57ab9fa7e7e5872789e9d2d35c54a59f
Philip <p208p2002@gmail.com>
2020-08-06 06:27:41 +0000

    custom tokenizer

---
+++ lib/tokenizer.py
@@ -0,0 +1,6 @@
+from transformers import BertTokenizer
+from .special_token_def import NO_ANSWER
+def init_tokenizer(model_name_or_vocab_path):
+    tokenizer = BertTokenizer.from_pretrained(model_name_or_vocab_path)
+    tokenizer._add_tokens([NO_ANSWER],special_tokens=True)
+    return tokenizer
--- train.py
+++ train.py
@@ -1,3 +1,6 @@
-from transformers import AlbertForMaskedLM,BertTokenizer
-# model = AlbertForMaskedLM.from_pretrained("voidful/albert_chinese_tiny")
-# tokenizer = BertTokenizer.from_pretrained("voidful/albert_chinese_tiny")
+from transformers import AlbertForMaskedLM
+from lib.tokenizer import init_tokenizer
+
+if __name__ == "__main__":
+    tokenizer = init_tokenizer("voidful/albert_chinese_tiny")
+    # model = AlbertForMaskedLM.from_pretrained("voidful/albert_chinese_tiny")
